7c0adef0dcd2041d439c993b15af8e193e1a7fe0
Lucy <thomalu@ssfss.org>
2020-02-19 14:41:03 -0500

    finished fraction

---
+++ src/ConsumerReviewLab.java
@@ -0,0 +1,7 @@
+public class ConsumerReviewLab {
+    public static void main(String[] args){
+        System.out.println(Review.sentimentVal("trouble"));
+        System.out.println(Review.sentimentVal("epic"));
+        System.out.println(Review.sentimentVal("incredible"));
+    }
+}
